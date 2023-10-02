bddcb118b2ae562466b0a7a04b69c1c43d3c91c3
Michał Fatyga <114155607+fatyga@users.noreply.github.com>
2023-10-02 20:17:41 +0200

    Add files via upload
    Add project's files created by Very Good CLI

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.eco_ideas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:eco_ideas/app/app.dart';
+import 'package:eco_ideas/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
